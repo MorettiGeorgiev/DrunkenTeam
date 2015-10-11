8c69f65a5e893dc6e0a4e48a4078290826619ec0
mbelchev <marianbelchev@gmail.com>
2015-10-11 13:28:59 +0300

    db

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2015 at 08:18 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Oct 11, 2015 at 12:28 PM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `junk`
+-- Database: `junco`
 --
 
 -- --------------------------------------------------------
@@ -48,7 +48,7 @@ INSERT INTO `admins` (`id`, `username`, `email`, `password`, `date-registered`) 
 --
 
 CREATE TABLE IF NOT EXISTS `signals` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `reg_number` varchar(8) NOT NULL,
   `photo` varchar(255) NOT NULL,
   `latFld` varchar(255) NOT NULL,
@@ -57,17 +57,19 @@ CREATE TABLE IF NOT EXISTS `signals` (
   `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `names` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+  `email` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'Сигналът е изпратен и очаква разглеждане.'
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `signals`
 --
 
-INSERT INTO `signals` (`id`, `reg_number`, `photo`, `latFld`, `lngFld`, `other_desc`, `date`, `names`, `phone`, `email`) VALUES
-(16, 'CB3818AC', '/DrunkenTeam/assets/uploads/44d88022b69f4b995067dde0d91eb959.jpg', '42.1485421894', '23.495165424', 'asd', '2015-06-07 16:57:50', 'Asd Asd Asd', '359878641143', 'marianbelchev@gmail.com'),
-(17, '', '/DrunkenTeam/assets/uploads/e1091bfa4669a50dbaccd4500b8247fa.jpg', '42.1376916415', '23.1491510826', '', '2015-06-07 16:58:27', 'Georgi L Velev', '+359895589829', 'george.velev1177@gmail.com'),
-(18, 'CB3818AC', '/DrunkenTeam/assets/uploads/1ad66cd9d186e538213d0a2346fabc6e.jpg', '42.641386808', '23.1454746601', 'Изоставена кола в тревните площи пред блока', '2015-06-07 17:36:43', 'Мариян Александров Белчев', '359878641143', 'marianbelchev@gmail.com');
+INSERT INTO `signals` (`id`, `reg_number`, `photo`, `latFld`, `lngFld`, `other_desc`, `date`, `names`, `phone`, `email`, `status`) VALUES
+(16, 'CB3818AC', '/DrunkenTeam/assets/uploads/44d88022b69f4b995067dde0d91eb959.jpg', '42.1485421894', '23.495165424', 'asd', '2015-06-07 16:57:50', 'Asd Asd Asd', '359878641143', 'marianbelchev@gmail.com', 'Junco се погрижи за вашият сигнал успешно'),
+(17, '', '/DrunkenTeam/assets/uploads/e1091bfa4669a50dbaccd4500b8247fa.jpg', '42.1376916415', '23.1491510826', '', '2015-06-07 16:58:27', 'Georgi L Velev', '+359895589829', 'george.velev1177@gmail.com', 'Сигналът е изпратен и очаква разглеждане.'),
+(18, 'CB3818AC', '/DrunkenTeam/assets/uploads/1ad66cd9d186e538213d0a2346fabc6e.jpg', '42.641386808', '23.1454746601', 'Изоставена кола в тревните площи пред блока', '2015-06-07 17:36:43', 'Мариян Александров Белчев', '359878641143', 'marianbelchev@gmail.com', 'Сигналът е изпратен и очаква разглеждане.'),
+(19, '-', '-', '42.678436', '23.292885399999932', '-', '2015-10-11 11:48:41', 'Marian Aleksandrov Belchev', '+359878641143', 'marianbelchev@gmail.com', 'Сигналът е изпратен и очаква разглеждане.');
 
 --
 -- Indexes for dumped tables
@@ -77,7 +79,7 @@ INSERT INTO `signals` (`id`, `reg_number`, `photo`, `latFld`, `lngFld`, `other_d
 -- Indexes for table `signals`
 --
 ALTER TABLE `signals`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -87,7 +89,7 @@ ALTER TABLE `signals`
 -- AUTO_INCREMENT for table `signals`
 --
 ALTER TABLE `signals`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=20;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
